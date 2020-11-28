98022dba254191e7ed39a47d573944744b9b8042
olegMarin <oleg.marin@riofi.ru>
2020-11-28 06:13:08 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import RusSVG from './svg/rus'
+import russia from './svg/russia.json'
+import colors from './styles/themes'
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      theme: 'light',
+      droped: [], //тут будут лежать все регионы, в которые дропнуты сущности данных
+      clicked: '', //тут лежит регион, по которому кликнули последний раз
+      regionsBackground: {}
+    }
+  }
+
+componentDidMount(){
+  this.changeRegionsBackground()
+}
+
+setTheme=(theme)=>{
+  this.changeRegionsBackground()
+  this.setState({theme: theme})
+}
+
+  changeRegionsBackground=()=>{
+    let newRegionsBackground = {}
+    let keyOfRegion = 0
+    while (russia.layers[keyOfRegion]){
+      let colorObj = {[russia.layers[keyOfRegion].id]: colors[this.state.theme].background}
+      newRegionsBackground = {...newRegionsBackground, ...colorObj}
+
+      keyOfRegion++
+    }
+    this.setState({ regionsBackground: newRegionsBackground})
+  }
+
+
+  handlerOnDropDataToRegion=(region)=>{
+    console.log(region)
+  }
+  handlerOnClickToRegion = (region) => {
+    this.setState({clicked: region})
+  }
+
+  render() {
+  let allActiveColors ={
+    background: this.state.regionsBackground,
+    whiteStroke: colors[this.state.theme].whiteStroke,
+    maneStroke: colors[this.state.theme].maneStroke,
+    backgroundHover: colors[this.state.theme].backgroundHover,
+    dragBackgrount: colors[this.state.theme].dragBackgrount,
+    dragStroke: colors[this.state.theme].dragStroke,
+    coastColor: colors[this.state.theme].coast,
+    backgroundClicked: colors[this.state.theme].backgroundClicked,
+    }
+    return (
+    <div {...this.props} style={{width: '100vw', height: '100vh'}}>
+        <RusSVG 
+          {...this.state} 
+          isOnDrop={(region)=>this.handlerOnDropDataToRegion(region)}
+          isOnClick={(region) => this.handlerOnClickToRegion(region)}
+          allActiveColors={allActiveColors}
+          />
+    </div>
+    )
+  }
+}
---
+++ src/styles/themes.js
@@ -0,0 +1,26 @@
+
+const colors = {
+    light: {
+        background: '#def',
+        whiteStroke: '#fff',
+        maneStroke: '#777',
+        backgroundHover: '#036780',
+        backgroundClicked: '#988',
+        dragBackgrount: '#050',
+        dragStroke: '#474',
+        coast: '#006cdf',
+
+    },
+    dark: {
+        background: '#000',
+        whiteStroke: '#fff',
+        maneStroke: '#fff6d5',
+        backgroundHover: '#033056',
+        backgroundClicked: '#900',
+        dragBackgrount: '#050',
+        dragStroke: '#474',
+        coast: '#00ccff',
+    }
+}
+
+export default colors
